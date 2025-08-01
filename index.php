8266972dfc6a05276a494cf762f4edb016e925bd
Fauzan <fauzan224321@gmail.com>
2025-08-01 23:01:55 +0700

    update: Using base_url

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+include_once __DIR__ . '/config/baseURL.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- Tailwindcss + DaisyUI -->
-     <link rel="stylesheet" href="./dist/css/output.css">
+     <link rel="stylesheet" href="<?= dist_url('css/output.css') ?>">
 </head>
 <body class="bg-gray-700 min-h-screen flex items-center justify-center">
     <h1 class="text-white text-bold text-7xl flex items-center justify-center">Tailwindcss</h1>
