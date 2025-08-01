4193c6e8e6f24da4f9fc675f299e78a5af9ba357
Fauzan <fauzan224321@gmail.com>
2025-08-01 23:01:20 +0700

    fix: Fixing path

--- config/baseURL.php
+++ config/baseURL.php
@@ -1,21 +1,21 @@
 <?php 
 
-$url = ''; // Sesuaikan dengan lokasi folder
+$url = 'https://localhost/git-project/kelompok-4/'; // Sesuaikan dengan lokasi folder
 
 if(!defined('BASE_URL')) {
     define('BASE_URL', $url);
 }
 
 function base_url($path = '') {
-    return BASE_URL . ltrim($path = '/');
+    return rtrim(BASE_URL, '/') . '/' . ltrim($path, '/');
 }
 
 function dist_url($path = '') {
-    return base_url('dist/' . ltrim($path = '/'));
+    return base_url('dist/' . ltrim($path, '/'));
 }
 
 function asset_url($path = '') {
-    return base_url('asset/' . ltrim($path = '/'));
+    return base_url('asset/' . ltrim($path, '/'));
 }
 
 ?>
